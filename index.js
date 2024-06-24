5bb4e88f21180f7baf1f57c754a3d5564fe57ee1
Ozgur <oeker55@gmail.com>
2024-06-24 11:08:56 +0300

    project set as commonjs

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-import fs from 'node:fs/promises'
-import express from 'express'
+const fs = require('fs').promises;
+const express = require('express');
 
-const port = process.env.PORT || 5173
-const base = process.env.BASE || '/'
+const port = process.env.PORT || 5173;
+const base = process.env.BASE || '/';
 
-const templateHtml = await fs.readFile('./dist/client/index.html', 'utf-8')
-// const ssrManifest = await fs.readFile('./dist/client/.vite/ssr-manifest.json', 'utf-8')
+const app = express();
 
-const app = express()
-const compression = (await import('compression')).default
-const sirv = (await import('sirv')).default
-app.use(compression())
-app.use(base, sirv('./dist/client', {extensions: []}))
+(async () => {
+    const templateHtml = await fs.readFile('./dist/client/index.html', 'utf-8');
+    const ssrManifest = await fs.readFile('./dist/client/.vite/ssr-manifest.json', 'utf-8');
 
-app.use('*', async (req, res) => {
-    try {
-        // const url = req.originalUrl.replace(base, '')
-        const template = templateHtml
-        const {render} = (await import('./dist/server/entry-server.mjs'))
-        const rendered = await render('Fr-5500191', 77404)
-        const script = `<script>window.__productDetailInitialState__ = ${JSON.stringify(rendered.productData).replace(/</g, '\\u003c')}</script>`
-        const html = template
-            .replace(`<!--app-head-->`, `${script}` ?? '')
-            .replace(`<!--app-html-->`, rendered.html ?? '')
+    const compression = require('compression');
+    const sirv = require('sirv');
 
-        res.status(200).set({'Content-Type': 'text/html'}).end(html);
+    app.use(compression());
+    app.use(base, sirv('./dist/client', { extensions: [] }));
 
-    } catch (e) {
-        console.log(e.stack)
-        res.status(500).end(e.stack)
-    }
-})
+    app.use('*', async (req, res) => {
+        try {
+            const template = templateHtml;
+            const { render } = await import('./dist/server/entry-server.mjs');
+            const rendered = await render('Fr-5500191', 77404);
+            const script = `<script>window.__productDetailInitialState__ = ${JSON.stringify(rendered.productData).replace(/</g, '\\u003c')}</script>`;
+            const html = template
+                .replace(`<!--app-head-->`, `${script}` ?? '')
+                .replace(`<!--app-html-->`, rendered.html ?? '');
 
-// Start http server
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`)
-})
+            res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+        } catch (e) {
+            console.log(e.stack);
+            res.status(500).end(e.stack);
+        }
+    });
+
+    // Start http server
+    app.listen(port, () => {
+        console.log(`Server started at http://localhost:${port}`);
+    });
+})();
